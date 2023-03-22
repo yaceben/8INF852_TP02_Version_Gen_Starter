5ba4d018abb6d14379318367f5aa31dc35a100c7
Yacine Benahmed <yacine_benahmed@uqar.ca>
2023-03-22 15:04:37 -0400

    premier commit avec code relativement fonctionnel

---
+++ main.py
@@ -0,0 +1,34 @@
+#%%
+import numpy as np
+import nltk
+from nltk.util import pad_sequence
+from nltk.lm.preprocessing import pad_both_ends
+from nltk.lm.preprocessing import flatten
+from nltk import FreqDist, ConditionalFreqDist
+from collections import defaultdict
+
+import gen_lm
+import generate_corpus
+
+
+
+#%%
+# voici une brève démonstration de l'utilisation du code fourni
+
+dictionnaire = generate_corpus.generate_dictionary() # peut prendre un peu de temps
+
+# dans notre cas, on va bâtir bêtement le corpus directement à partir du dictionnaire traité
+corpus_entrainement = dictionnaire
+
+
+trigram_model = gen_lm.build_trigram_model(corpus_entrainement)
+
+mots = ['bonjour', 'jourbon', 'manger']
+
+
+# %%
+for mot in mots:
+    ppl = gen_lm.perplexité(mot=mot, trigram_model=trigram_model)
+    print(f"{mot}: {ppl}")
+
+# %%
